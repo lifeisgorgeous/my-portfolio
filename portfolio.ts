4e32fbc665f8140eccfe98e6656decfbee7de827
solaimanhridoy <solaimanhridoy@gmail.com>
2023-07-29 12:21:22 +0600

    Updated website

--- portfolio.ts
+++ portfolio.ts
@@ -12,181 +12,87 @@ import {
 } from './types/sections';
 
 export const greetings: GreetingsType = {
-  name: 'Hanzla Tauqeer',
-  title: "Hi all, I'm Hanzla",
+  name: 'Solaiman Hridoy',
+  title: "Hi all, I'm Solaiman",
   description:
-    "I'm passionate Full Stack web developer having an experience of web applications with Python, Django, React.js, Next.js and Blockchain development on Ethereum, Solidity, Web3.js, Moralis, and Brownie Framework.",
+    "I'm a passionate Data Analyst having specialties in Data Analysis, Programming using Python, R, and SQL, Spreadsheets, Data Visualization using Looker Studio, Tableau, Youbora and Web Analytics using Google Analytics 4.",
   resumeLink:
-    'https://docs.google.com/document/d/1_4RIfbyt4FFHW_bNplnoINQr01neTu7XDeeUlT_2O_k/edit?usp=sharing',
+    'https://www.canva.com/design/DAExkMYMIDw/L0ryYqGCOjf0AAYrdh_0Gg/view?utm_content=DAExkMYMIDw&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink',
 };
 
 export const openSource = {
-  githubUserName: '1hanzla100',
+  githubUserName: 'solaimanhridoy',
 };
 
 export const contact = {};
 
 export const socialLinks: SocialLinksType = {
-  url: 'https://1hanzla100.github.io/',
-  linkedin: 'https://www.linkedin.com/in/1hanzla100/',
-  github: 'https://github.com/1hanzla100',
-  instagram: 'https://www.instagram.com/__hanzla100',
-  facebook: 'https://www.facebook.com/1hanzla100',
-  twitter: 'https://twitter.com/1hanzla100',
+  url: 'https://solaimanhridoy.com/',
+  linkedin: 'https://www.linkedin.com/in/solaimanhridoy/',
+  github: 'https://github.com/solaimanhridoy',
+  instagram: 'https://www.instagram.com/solaimanhrid0y',
+  facebook: 'https://www.facebook.com/solaimanhrid0y',
+  twitter: 'https://twitter.com/solaimanhridoy',
 };
 
 export const skillsSection: SkillsSectionType = {
   title: 'What I do',
-  subTitle: 'CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK',
+  subTitle: 'An Analyst Who Loves To Explore Tech and Data',
   data: [
     {
-      title: 'Full Stack Development',
-      lottieAnimationFile: '/lottie/skills/fullstack.json', // Path of Lottie Animation JSON File
+      title: 'Data, Information, Insight',
+      lottieAnimationFile: '/lottie/skills/cloudinfra.json', // Path of Lottie Animation JSON File
       skills: [
         emoji(
-          '⚡ Building responsive Single-Page-Apps (SPA) & PWA in React.js'
+          '⚡ Automating reports and dashboards.'
         ),
-        emoji('⚡ Building responsive static websites using Next.js'),
-        emoji('⚡ Building RESTful APIs in Django & Django REST Framework'),
+        emoji('⚡ Providing insights and recommendations from campaign data.'),
+        emoji('⚡ Communicating technical results and collaborate cross-functionally.'),
       ],
       softwareSkills: [
         {
-          skillName: 'HTML-5',
-          iconifyTag: 'vscode-icons:file-type-html',
-        },
-        {
-          skillName: 'CSS-3',
-          iconifyTag: 'vscode-icons:file-type-css',
-        },
-        {
-          skillName: 'JavaScript',
-          iconifyTag: 'logos:javascript',
-        },
-        {
-          skillName: 'Reactjs',
-          iconifyTag: 'vscode-icons:file-type-reactjs',
-        },
-        {
-          skillName: 'Nextjs',
-          iconifyTag: 'vscode-icons:file-type-light-next',
+          skillName: 'Statistics',
+          iconifyTag: 'noto-v1:input-numbers',
         },
         {
           skillName: 'Python',
           iconifyTag: 'logos:python',
         },
         {
-          skillName: 'Django',
-          iconifyTag: 'vscode-icons:file-type-django',
-        },
-
-        {
-          skillName: 'Redux',
-          iconifyTag: 'logos:redux',
-        },
-        {
-          skillName: 'NPM',
-          iconifyTag: 'logos:npm-icon',
-        },
-        {
-          skillName: 'Yarn',
-          iconifyTag: 'logos:yarn',
-        },
-      ],
-    },
-    {
-      title: 'Cloud Infra-Architecture',
-      lottieAnimationFile: '/lottie/skills/cloudinfra.json', // Path of Lottie Animation JSON File
-      skills: [
-        emoji('⚡ Experience of working on multiple cloud platforms'),
-        emoji(
-          '⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases'
-        ),
-        emoji(
-          '⚡ Building CI/CD pipelines for automated testing & deployment using Github Actions'
-        ),
-      ],
-      softwareSkills: [
-        // ? Check README To get icon details
-        {
-          skillName: 'AWS',
-          iconifyTag: 'logos:aws',
-        },
-        {
-          skillName: 'Azure',
-          iconifyTag: 'logos:microsoft-azure',
-        },
-        {
-          skillName: 'Heroku',
-          iconifyTag: 'logos:heroku-icon',
+          skillName: 'R',
+          iconifyTag: 'vscode-icons:file-type-r',
         },
         {
           skillName: 'PostgreSQL',
-          iconifyTag: 'logos:postgresql',
+          iconifyTag: 'vscode-icons:file-type-pgsql',
+        },
+        {
+          skillName: 'Excel or Spreadsheet',
+          iconifyTag: 'vscode-icons:file-type-excel2',
+        },
+        {
+          skillName: 'Looker or Data Studio',
+          iconifyTag: 'logos:google-data-studio',
+        },
+        {
+          skillName: 'Tableau',
+          iconifyTag: 'logos:tableau-icon',
+        },
+        {
+          skillName: 'Presentation',
+          iconifyTag: 'vscode-icons:file-type-powerpoint',
+        },
+        {
+          skillName: 'Canva',
+          iconifyTag: 'devicon:canva',
+        },
+        {
+          skillName: 'GA4',
+          iconifyTag: 'logos:google-analytics',
         },
         {
           skillName: 'Github',
           iconifyTag: 'akar-icons:github-fill',
-        },
-        {
-          skillName: 'Docker',
-          iconifyTag: 'logos:docker-icon',
-        },
-        {
-          skillName: 'Github Actions',
-          iconifyTag: 'logos:github-actions',
-        },
-        {
-          skillName: 'Cloudinary',
-          iconifyTag: 'logos:cloudinary',
-        },
-        {
-          skillName: 'Nginx',
-          iconifyTag: 'logos:nginx',
-        },
-        {
-          skillName: 'Sentry',
-          iconifyTag: 'logos:sentry-icon',
-        },
-      ],
-    },
-    {
-      title: 'Blockchain',
-      lottieAnimationFile: '/lottie/skills/ethereum.json', // Path of Lottie Animation JSON File
-      skills: [
-        emoji(
-          '⚡ Experience in developing Smart Contract using Solidity & Ethereum'
-        ),
-        emoji(
-          '⚡ Building Scripts for automated testing & deployment of Smart Contracts using Brownie & Infura'
-        ),
-        emoji(
-          '⚡ Experience of using Openzeppelin Smart Contract Standards & Chainlink Oracles'
-        ),
-        emoji('⚡ Developing NFT Smart Contracts using ERC-721 Token Standard'),
-        emoji(
-          '⚡ Building Dapps with React.js & Solidity using Web3.js, Moralis & IPFS'
-        ),
-      ],
-      softwareSkills: [
-        {
-          skillName: 'Ethereum',
-          iconifyTag: 'logos:ethereum',
-        },
-        {
-          skillName: 'Solidity',
-          iconifyTag: 'logos:solidity',
-        },
-        {
-          skillName: 'Web3js',
-          iconifyTag: 'logos:web3js',
-        },
-        {
-          skillName: 'Metamask',
-          iconifyTag: 'logos:metamask-icon',
-        },
-        {
-          skillName: 'Ganache',
-          iconifyTag: 'logos:ganache-icon',
         },
       ],
     },
@@ -195,123 +101,133 @@ export const skillsSection: SkillsSectionType = {
 
 export const SkillBars: SkillBarsType[] = [
   {
-    Stack: 'Frontend/Design', //Insert stack or technology you have experience in
+    Stack: 'Data Collecting and Preprocessing', //Insert stack or technology you have experience in
     progressPercentage: '90', //Insert relative proficiency in percentage
   },
   {
-    Stack: 'Backend',
-    progressPercentage: '70',
+    Stack: 'Data Visualization',
+    progressPercentage: '80',
   },
   {
     Stack: 'Programming',
-    progressPercentage: '60',
+    progressPercentage: '70',
   },
 ];
 
 export const educationInfo: EducationType[] = [
   {
-    schoolName: 'Harvard University',
-    subHeader: 'Master of Science in Computer Science',
-    duration: 'September 2017 - April 2019',
-    desc: 'Participated in the research of XXX and published 3 papers.',
-    grade: 'Grade A',
+    schoolName: 'University of Science and Technology Chittagong',
+    subHeader: 'Bachelor of Science in Computer Science and Engineering',
+    duration: 'Jan 2017 - November 2021',
+    desc: 'Competitive Programming Contestant and Participated in the research works.',
+    grade: 'Grade B+',
     descBullets: [
-      'Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit',
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      'Inter-University Programming Contestant',
+      'ICPC Regional Dhaka Participant (USTC_BLACK), 2018',
+      'Runner-Up Team (USTC_BLACK), Intra-Faculty Programming Contest, 2018',
+      'Asst. Media & Communication Secretary, Executive Member, Graphics Designer at FSET Research Club, USTC, 2018-2020',
     ],
   },
 ];
 
 export const experience: ExperienceType[] = [
   {
-    role: 'Frontend Developer',
-    company: 'Duseca Software',
-    companyLogo: '/img/icons/common/dusecaSoftware.jpg',
-    date: 'Apr 2022 – Jun 2022',
-    desc: 'I worked as frontend web developer to design web based admin dashboards for mulitple apps using Django, Bootstrap, jQuery, Chart.js and some other libraries.',
+    role: 'Senior Analytics Executive',
+    company: 'Bongo (Stellar Digital Limited)',
+    companyLogo: '/img/icons/common/bongo.png',
+    date: 'Feb 2023 – Present',
+    desc: 'Skills: Web Analytics · Data Analytics · Dashboard · Analytical Skills · Google Data Studio · Video Analytics · Google Sheets · Google Analytics · Problem Solving · Tableau',
+    descBullets: [
+      "Helping stakeholders make better business decisions by analyzing complex data and identifying problem areas. ",
+      "Generating comprehensive reports summarizing campaign data and key insights, enabling the team to quickly identify areas for improvement and make data-driven decisions.",
+      "Collaborating with cross-functional teams to collect, analyze, and interpret data from multiple platforms, resulting in the development of actionable recommendations for clients and improved campaign performance.",
+    ],
   },
   {
-    role: 'API Engineer',
-    company: 'Duseca Software',
-    companyLogo: '/img/icons/common/dusecaSoftware.jpg',
-    date: 'Jan 2022 – Mar 2022',
-    desc: 'I worked as API Engineer on a Kale App. Kale is a community platform for the creation of social reviews based on verified transactions. I built Fully Optimized and High Performance RESTful APIs using Django, DjangoRESTFramework, PostgresSQL, JWT Authentication, AWS, Integrated Plaid APIs for Fetching Bank Transactions, Integrated Sentry for monitoring Django Server and deployed the API on Heroku Server.',
-    // descBullets: [
-    // 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
-    // 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    // ],
+    role: 'Analytics Executive',
+    company: 'Bongo (Stellar Digital Limited)',
+    companyLogo: '/img/icons/common/bongo.png',
+    date: 'Feb 2022 – Jan 2023',
+    desc: 'Skills: Web Analytics · Data Analytics · Dashboard · Analytical Skills · Google Data Studio · Video Analytics · Google Sheets · Microsoft Excel · Google Analytics · Python (Programming Language) · Problem Solving · Data Analysis · Data Visualization · SQL · PostgreSQL',
+    descBullets: [
+      "Successfully automated monthly and weekly content reports and dashboards, streamlining reporting processes and saving the team valuable time and resources.",
+      "Generated comprehensive reports summarizing campaign data and key insights, enabling the team to quickly identify areas for improvement and make data-driven decisions.",
+      "Collaborated with cross-functional teams to collect, analyze, and interpret data from multiple platforms, resulting in the development of actionable recommendations for clients and improved campaign performance.",
+    ],
   },
   {
-    role: 'Full Stack Developer',
-    company: 'Bleed-AI',
-    companyLogo: '/img/icons/common/bleedAI.jpg',
-    date: 'Sept 2021 - Oct 2021',
-    desc: 'Worked as django web developer to create a website in which I integrated computer vision AI model (built by the team of BleedAI) to process YouTube URLs and show Real Time processing on Website using Django Channels WebSockets , threading for running multiple instances of AI Model, jQuery for UI rendering and deployed it on Heroku server.',
+    role: 'Data Anlyst Intern',
+    company: 'Upskill Bangladesh',
+    companyLogo: '/img/icons/common/upskill.jpeg',
+    date: 'Dec 2021 - Jan 2022',
+    desc: 'Google Sheets · Tableau · Microsoft Excel · Google Analytics · Problem Solving · Data Analysis · Data Visualization',
+    descBullets: [
+      'Performing analysis of EdTech project datasets including data collection, data management, data cleanup & data visualization',
+      'Implementation of web analytics by setting goals and tracking funnels on google analytics.',
+      'Creating reports by analyzing historical data to drive business insights and communicate findings with the core team.',
+    ],
   },
   {
-    role: 'Backend Developer',
-    company: 'Wapidu',
-    companyLogo: '/img/icons/common/wapidu.jpg',
-    date: 'Sept 2021',
-    desc: 'Worked as a Django Developer to integrate Stripe payment gateway in wapidu.com and Created REST APIs using Django REST Framework to integrate Django Stripe backend to Vue.js Frontend and Deployed it on Azure based Docker container registry.',
+    role: 'Full Stack Web Developer Intern',
+    company: 'Rfera Technology',
+    companyLogo: '/img/icons/common/rfera.jpeg',
+    date: 'Mar 2021 - June 2021',
+    desc: 'Skills: Vuex · Django REST Framework · Internet of Things (IoT) · Sigfox · Python (Programming Language) · Problem Solving · JavaScript · Django · SQL · PostgreSQL · Vue.js',
+    descBullets: [
+      "Writing the source code and documentation for an IOT-based project using Python, Django, and Vuejs.",
+      "Testing API with Postman Tool",
+      "Aligning with the company's core values and other disciplines by teamwork."
+    ],
   },
 ];
 
 export const projects: ProjectType[] = [
   {
-    name: 'developer-portfolio',
-    desc: 'Software Developer Portfolio Template built with react.js and next.js bootstrap that helps you showcase your work and skills as a software developer.',
-    github: 'https://github.com/1hanzla100/developer-portfolio',
-    link: 'https://developer-portfolio-1hanzla100.vercel.app/',
+    name: 'Covid 19 Data Exploration',
+    desc: 'Analysis of Covid-19',
+    github: 'https://github.com/solaimanhridoy/covid-19-data-exploration',
   },
   {
-    name: 'AtlasMart',
-    desc: 'With Atlas Mart, it’s easy to find the products with the best price and ship them straight to your door.',
-    github: 'https://github.com/1hanzla100/Django-React-Marketplace',
+    name: 'CO2 Emission By Country',
+    desc: 'Visulaizations of CO2 Emission By Country',
+    link: 'https://public.tableau.com/shared/MNQP4MHDZ?:display_count=n&:origin=viz_share_link',
   },
   {
-    name: 'Technota (Forum)',
-    desc: 'Get hands-on experience in technical skills with Technota',
-    github: 'https://github.com/1hanzla100/django-react-forum',
-  },
-  {
-    name: 'Shopaza (Ecommerce)',
-    desc: 'Now your reach to all your favorite entertainment and luxury items is just one touch apart',
-    github: 'https://github.com/1hanzla100/Django-ecommerce',
+    name: 'Cyclistic Bike Share',
+    desc: 'Google Data Anlytics Specialities Hands On Porject',
+    github: 'https://github.com/solaimanhridoy/cyclistic-bike-share/blob/master/cyclistic-bike-share.ipynb',
   },
 ];
 
-export const feedbacks: FeedbackType[] = [
-  {
-    name: 'John Smith',
-    feedback:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
-  },
-  {
-    name: 'John Smith',
-    feedback:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
-  },
-];
+// export const feedbacks: FeedbackType[] = [
+//   {
+//     name: 'John Smith',
+//     feedback:
+//       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
+//   },
+//   {
+//     name: 'John Smith',
+//     feedback:
+//       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
+//   },
+// ];
 
 // option to hide or show the ContactUs component
 export const showContactUs: boolean = true;
 
 // See object prototype on /types/section.ts page
 export const seoData: SEODataType = {
-  title: 'Hanzla Tauqeer',
+  title: 'Solaiman Hridoy',
   description:
     'A passionate Full Stack Web Developer and Blockchain Developer.',
-  author: 'Hanzla Tauqeer',
+  author: 'Solaiman Hridoy',
   image: 'https://avatars3.githubusercontent.com/u/59178380?v=4',
   url: 'https://developer-portfolio-1hanzla100.vercel.app',
   keywords: [
-    'Hanzla',
-    'Hanzla Tauqeer',
-    '@1hanzla100',
-    '1hanzla100',
+    'Solaiman',
+    'Solaiman Hridoy',
     'Portfolio',
-    'Hanzla Portfolio ',
-    'Hanzla Tauqeer Portfolio',
+    'Solaiman Portfolio ',
+    'Solaiman Hridoy Portfolio',
   ],
 };
